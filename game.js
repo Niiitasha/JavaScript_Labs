7f71418266af08f4913a5ebe39e8459dac02789b
N D <detroitmuseumofhealing@gmail.com>
2017-10-22 19:51:49 -0400

    revisedgame

--- game.js
+++ game.js
@@ -1,26 +1,46 @@
-//Variables:
-
-var player = 40;
-var almightyG = 10;
-
-
 //Game:
 
 var playGame = (prompt("The Almighty Grant challenges you to a game! Do you want to play?"));
 
-if (playGame === "yes"){
+if (playGame === "yes" || playGame === "Yes"){
    var playerName = (prompt("What is your name?"));
- }
 
-while (player > 0 && almightyG > 0){
-  (player -= (Math.floor(Math.random() * 2) + 1));
-  (almightyG -= (Math.floor(Math.random() * 2) + 1));
-  console.log("The Almighty Grant's score is " + almightyG);
-  console.log(playerName + "'s score is " + player);
+   //Variables:
 
-  if (almightyG <= 0 || player <= 0){
+   var player = 40;
+   var almightyG = 10;
+   var games = 3;
+
+   while (player > 0 && almightyG > 0){
+   (player -= (Math.floor(Math.random() * 2) + 1));
+   (almightyG -= (Math.floor(Math.random() * 2) + 1));
+   console.log("The Almighty Grant's score is " + almightyG);
+   console.log(playerName + "'s score is " + player);
+
+   if (almightyG <= 0 && games > 1){
     almightyG = 10;
-    //counter for # of games??? 
- }
+    games--;
+    console.log("NEXT ROUND STARTING:")
+   }else if (player === 0 || games < 0){
+    console.log("GAME OVER");
+   }
+   }
 
-}
+   if (player > 0 && almightyG === 0){
+    console.log("You win!");
+   } else {
+    console.log("You can't beat the Almighty Grant!");
+   }
+
+
+
+
+
+
+
+
+
+
+ }else{
+   console.log("BYE THEN.");
+ }
