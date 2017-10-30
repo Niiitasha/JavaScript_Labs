f66700c4dcb168a6a0a2fa2a54b6f48e669da536
N D <detroitmuseumofhealing@gmail.com>
2017-10-30 01:25:40 -0400

    game edits

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
-//Game:
+//Game with DOM:
 
 var start = document.getElementById("start");
-var playerName = "player";
+var playerName = "Player";
 
 start.onclick = function startGame(){
   document.getElementById("start").style.display = "none";
-  var playerName = (prompt("Please enter your name:"));
+  var pName = (prompt("Please enter your name:"));
+  //document.getElementById("name").textContent = ( pName + ("'s") );
+  document.getElementById("you").textContent = ( pName + ("'s") );
   document.getElementById("second_page").style.display = "block";
+  document.getElementById("scoring").textContent = "";
+  return almightyG.health = 10;
+  return player.health = 40;
 }
 
 //Attack Button:
@@ -14,30 +19,49 @@ var startAttack = document.getElementById("attack");
 startAttack.onclick = function startCombat() {
   document.getElementById("pScore").value = ( player.health -= almightyG.generateAttackDamage() );
   document.getElementById("gScore").value = ( almightyG.health -= player.generateAttackDamage() );
-  console.log(player.health, almightyG.health);
-  document.getElementById("playerScore").innerHTML = player.health;
-  document.getElementById("grantScore").innerHTML = almightyG.health;
+  document.getElementById("playerScore").textContent = player.health;
+  document.getElementById("grantScore").textContent = almightyG.health;
+  document.getElementById("keepUScore").textContent = player.health;
+  document.getElementById("keepGScore").textContent = almightyG.health;
+  document.getElementById("grantScore").value = almightyG.health;
+  document.getElementById("pHealth").value = player.health;
+  if (almightyG.health <= 0 && player.health >= 0 && player.wins < 3){
+    return almightyG.health = 10;
+    player.wins++;
+    document.getElementById("wins").value = player.wins;
+  } else if (player.health <= 0 && almightyG > 0) {
+    document.getElementById("scoring").textContent = "YOU LOSE!";
+    document.getElementById("second_page").style.display = "none";
+    document.getElementById("start").style.display = "inline-block";
+  }
 }
 
 //Heal Button:
 var healing = document.getElementById("heal");
 healing.onclick = function () {
   player.heal();
-  document.getElementById("playerScore").innerHTML = player.health;
-  document.getElementById("pScore").value = player.health;
-  console.log(player.health);
+  player.healCount++;
+  if (player.healCount < 3){
+  document.getElementById("playerScore").textContent = player.health;
+  document.getElementById("keepUScore").textContent = player.health;
+  document.getElementById("pHealth").value = ( player.health -= almightyG.generateAttackDamage() );
+} else {
+  document.getElementById("scoring").textContent = "You ran out of medicine and can no longer heal.";
+}
 }
 
 //Quit Button:
 
 var quitter = document.getElementById("quit");
   quitter.onclick = function () {
-  document.getElementById("scoring").innerHTML = "Winners never quit and quitters never win.";
+  document.getElementById("second_page").style.display = "none";
+  document.getElementById("start").style.display = "inline-block";
+  document.getElementById("scoring").textContent = "Winners never quit and quitters never win.";
 }
 
 //Player Object:
 var player = {
-name: null,
+name: "",
 health: 40,
 wins: 0,
 healCount: 0,
@@ -53,92 +77,3 @@ name: "The Almighty Grant",
 health: 10,
 generateAttackDamage: function() { return Math.floor(Math.random() * 5) + 1}
 };
-
-
-
-
-/*Game refactored with Objects:
-
-function playGame(){
-
-  var playGame = (prompt("The Almighty Grant challenges you to a game! Do you want to play?"));
-  if (playGame === "yes" || playGame === "Yes" || playGame === "YES"){
-  var playerName = (prompt("What is your name?"));
-  player.name = playerName;
- startCombat(playerName);
-  } else {
-  console.log("BYE THEN");
-}
-}
-
-//Player Object
-var player = {
-//Properties
-name: null,
-health: 40,
-wins: 0,
-healCount: 0,
-//Methods
-generateAttackDamage: function() {
-  return Math.floor(Math.random() * 3) + 1},
-heal: function(){
-  return this.health += Math.floor(Math.random() * 10) + 1;}
-};
-
-//AlmightyG Object
-var almightyG = {
-//Properties
-name: "The Almighty Grant",
-health: 10,
-generateAttackDamage: function() { return Math.floor(Math.random() * 5) + 1}
-};
-
-playGame();
-
-//Start Combat:
-
-function startCombat(playerName){
-
-while (player.health > 0 && almightyG.health > 0 && player.wins < 3){
-
-   var choice = (prompt("ATTACK, HEAL OR QUIT?"));
-   if (choice === "quit" || choice === "Quit" || choice === "QUIT"){
-      console.log("Winners never quit and quitters NEVER WIN");
-      break;
-
-   } else if(choice === "attack" || choice === "Attack" || choice === "ATTACK"){
-     player.health -= almightyG.generateAttackDamage();
-     almightyG.health -= player.generateAttackDamage();
-     console.log("The Almighty Grant's score is " + almightyG.health);
-     console.log(player.name + "'s score is " + player.health);
-
-   } else if (choice === "heal" && player.healCount <= 1){
-       player.healCount++;
-       player.heal();
-       console.log(player.name + ", you have healed to a new score of " + player.health);
-       player.health -= almightyG.generateAttackDamage();
-       almightyG.health -= player.generateAttackDamage();
-       console.log("The Almighty Grant's current score is " + almightyG.health);
-       console.log(player.name + "'s current score is " + player.health);
-
-  } else if (player.healCount >= 2){
-       console.log("You have no more medicine, healing is not an option.")
-  }
-
-  if (almightyG.health <= 0 && player.health > 0 && player.wins < 2){
-      almightyG.health = 10;
-      player.wins++;
-      console.log("NEXT ROUND");
-
-  }else if (almightyG.health === 0 && player.wins === 2){
-    console.log("Somehow you have beat the Almighty Grant.")
-    break;
-  }
-
-  if (player.health <= 0 && almightyG > 0){
-      console.log("GAME OVER, YOU LOSE!");
-      break;
-  }
-  }
-}
-*/
